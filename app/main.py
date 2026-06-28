f56b1d047bfac9a3d3f560612d850fcebeeba5e3
Swayam Randhe <166550144+randheswayam@users.noreply.github.com>
2026-06-28 20:29:49 +0530

    Update main.pysolving error for main.py

--- app/main.py
+++ app/main.py
@@ -1,26 +1,30 @@
-import streamlit as st 
+import streamlit as st
 from router import router
-from faq import ingest_faq_data, faq_chain
+from faq import ingest_faq_data,faq_chain
 from pathlib import Path
 from sql import sql_chain
+
 
 faqs_path = Path(__file__).parent / "resources/faq_data.csv"
 ingest_faq_data(faqs_path)
 
-col1,cal2 = st.columns([1,5])
-with col1: 
-    st.image("img.png",width=80)
-with cal2:
-    st.title("E Commerce Chatbot")
-
 def ask(query):
     route = router(query).name
-    if route=='faq':
+    if route == 'faq':
         return faq_chain(query)
     elif route == 'sql':
         return sql_chain(query)
     else:
-        return "Rout {route} not implemented yet "
+        return f"Route {route} not implemented yet"
+
+# st.image("img.png", width=50)
+# st.title("E commerce chatbot")
+
+col1, col2 = st.columns([1, 5])
+with col1:
+    st.image("img.png", width=80)
+with col2:
+    st.title("E Commerce Chatbot")
 
 query = st.chat_input("Write your query")
 
@@ -34,11 +38,11 @@ for message in st.session_state.messages:
 if query:
     with st.chat_message("user"):
         st.markdown(query)
-    st.session_state.messages.append({"role": "user" , "content" : query})
-
+    st.session_state.messages.append({"role":"user", "content": query})
 
     response = ask(query)
     with st.chat_message("assistant"):
         st.markdown(response)
-    st.session_state.messages.append({"role": "assistant" , "content" : response})
-    
+    st.session_state.messages.append({"role":"assistant", "content": response})
+
+
